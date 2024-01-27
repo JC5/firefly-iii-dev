f122fcf827c197f118e70b1c01f61747886d5c93
James Cole <james@firefly-iii.org>
2024-01-27 14:22:58 +0100

    Expand script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,26 @@
 
 echo php -v
 
-php cli.php
+pwd
+ls
+
+cd /usr/src/dev-tools
+
+pwd
+ls
+
+
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
-sudo mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
 
 composer install
+
+php cli.php
 
 echo "Hello $1"
 time=$(date)
