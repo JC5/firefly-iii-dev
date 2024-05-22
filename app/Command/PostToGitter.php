8c48270cfe35f999aeabddee9f72c0df3ea115a5
James Cole <james@firefly-iii.org>
2024-05-22 06:02:33 +0200

    Add ability to deal with PR

--- app/Command/PostToGitter.php
+++ app/Command/PostToGitter.php
@@ -45,6 +45,10 @@ class PostToGitter extends Command
             $this->postIssue($output);
             return 0;
         }
+        if('pr' === $type) {
+            $this->postPullRequest($output);
+            return 0;
+        }
         $output->writeln(sprintf('Did not recognize issue type "%s", no post was made.', $type));
         return 0;
     }
@@ -73,6 +77,52 @@ class PostToGitter extends Command
 
         $message     = sprintf('🤖 On GitHub, a new issue was opened by **%s**: "[%s](https://github.com/%s/issues/%s)"', $user, $title, $repository, $number);
         $messageHtml = sprintf('🤖 On GitHub, a new issue was opened by <b>%s</b>: "<a href="https://github.com/%s/issues/%s" title="%s">%s</a>"', $user, $repository, $number, $title, $title);
+
+
+        $res = $client->put(sprintf($full, $host, $this->room, time()), [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                "Accept"        => 'application/json',
+            ],
+            'body'    => json_encode(
+                [
+                    'body'           => $message,
+                    'format'         => 'org.matrix.custom.html',
+                    'formatted_body' => $messageHtml,
+                    'msgtype'        => 'm.text',
+                ]),
+        ]);
+        $output->writeln(sprintf('Result of PUT: HTTP %d', $res->getStatusCode()));
+    }
+
+    private function postPullRequest(OutputInterface $output): void
+    {
+        $output->writeln('Will post about an issue to Gitter.');
+        $number     = (string) (getenv('PR_NUMBER') ?? '');
+        $repository = (string) (getenv('REPOSITORY') ?? '');
+        $title      = (string) (getenv('PR_TITLE') ?? '');
+        $user       = (string) (getenv('PR_USER') ?? '');
+        $token      = (string) (getenv('GITTER_TOKEN') ?? '');
+
+        if ('' === $number) {
+            $output->writeln('No PR number provided.');
+            return;
+        }
+        if('dependabot' === $user || 'dependabot[bot]' === $user) {
+            $output->writeln(sprintf('Will ignore user "%s".', $user));
+            return;
+        }
+        $output->writeln(sprintf('PR number: #%s', $number));
+        $output->writeln(sprintf('PR title: %s', $title));
+        $output->writeln(sprintf('PR user: %s', $user));
+        $output->writeln(sprintf('GitHub repository: %s', $repository));
+
+        $full   = 'https://%s/_matrix/client/v3/rooms/%s/send/m.room.message/%s';
+        $host   = 'gitter.ems.host';
+        $client = new Client();
+
+        $message     = sprintf('🤖 On GitHub, a new PR was opened by **%s**: "[%s](https://github.com/%s/pull/%s)"', $user, $title, $repository, $number);
+        $messageHtml = sprintf('🤖 On GitHub, a new PR was opened by <b>%s</b>: "<a href="https://github.com/%s/pull/%s" title="%s">%s</a>"', $user, $repository, $number, $title, $title);
 
 
         $res = $client->put(sprintf($full, $host, $this->room, time()), [
